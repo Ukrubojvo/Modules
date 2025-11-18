ff20b05c727ca8b9098caaaeeaea6a4ec80260f2
Jowhqvfj <138446145+Ukrubojvo@users.noreply.github.com>
2025-11-18 21:18:53 +0900

    Update StaffDetector.lua

--- StaffDetector.lua
+++ StaffDetector.lua
@@ -1,7 +1,7 @@
 xpcall(function()
     if shared.StaffDetectorLoading then return end
     shared.StaffDetectorLoading = true
-    game.Loaded:Wait()
+    repeat task.wait() until game:IsLoaded()
     local cloneref = cloneref or function(obj)
         return obj
     end
