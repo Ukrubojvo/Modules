acb4a0ab31f168cbfc98073821ef037df4043c72
AntiLua <138446145+Ukrubojvo@users.noreply.github.com>
2026-02-11 19:08:29 +0900

    Update Millenium.lua

--- Millenium.lua
+++ Millenium.lua
@@ -635,7 +635,7 @@
                     Parent = library[ "items" ];
                     Size = cfg.size;
                     Name = "\0";
-                    --Position = dim2(0.5, 0, 0.5, 0);
+                    Position = dim2(0.5, -cfg.size.X.Offset / 2, 0.5, -cfg.size.Y.Offset / 2);
                     AnchorPoint = vec2(0.5, 0.5);
                     BorderColor3 = rgb(0, 0, 0);
                     BorderSizePixel = 0;
