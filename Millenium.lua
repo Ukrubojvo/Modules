d912e761a3eff69148e99bdedfe92c5bea07b022
RB-DaY <yh99484@gmail.com>
2026-04-09 00:04:17 +0900

    test

--- Millenium.lua
+++ Millenium.lua
@@ -741,7 +741,7 @@
                 items["title"] = library:create("ViewportFrame", {
                     Parent = items["side_frame"];
                     Name = "\0";
-                    Size = dim2(0.3, 0, 0, 70);
+                    Size = dim2(0.4, 0, 0, 70);
                     BackgroundTransparency = 0;
                     BorderSizePixel = 0;
                     BackgroundColor3 = rgb(16, 16, 16);
@@ -763,7 +763,7 @@
                     Text = string.format('%s<font color = "rgb(204, 204, 204)">%s</font>', cfg.name, cfg.suffix);
                     BackgroundTransparency = 1;
                     Size = dim2(0.7, 0, 0, 70);
-                    Position = dim2(0.3, 0, 0, 0);
+                    Position = dim2(0.55, 0, 0, 0);
                     TextColor3 = themes.preset.accent;
                     BorderSizePixel = 0;
                     RichText = true;
